8417dd990e00f7bd5ebb442e9e36f9417fddc034
lukasz protosawicki <lukas.es.p@o2.pl>
2021-06-15 15:29:57 +0200

    add react components

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,14 @@
 import React from "react";
+import AddNewProject from "./AddNewProject";
+import Project from "./Project";
 
 function Projects() {
-  return <div className="Projects">Projects</div>;
+  return (
+    <div className="Projects">
+      <AddNewProject />
+      <Project />
+    </div>
+  );
 }
 
 export default Projects;
