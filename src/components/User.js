f1c87f737328cdb9b95c51373b1020e46553b841
lukasz protosawicki <lukas.es.p@o2.pl>
2021-06-15 16:10:55 +0200

    add bg, logo and styles

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,18 @@
 import React from "react";
+import logo from "../images/logo.png";
 
 function User() {
-  return <div className="User">User</div>;
+  return (
+    <div className="User">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="info">
+        <p>Todo App</p>
+        <a href="#">Logout!</a>
+      </div>
+    </div>
+  );
 }
 
 export default User;
