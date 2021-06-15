c5433bb7ba946c2fbeaa7e5542d414fd12bfb07f
lukasz protosawicki <lukas.es.p@o2.pl>
2021-06-15 18:35:41 +0200

    add mixed + add DatePicker and TimePicker

--- src/components/AddNewTodo.js
+++ src/components/AddNewTodo.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 import Modal from "./Modal";
-import { Bell, CalendarDay } from "react-bootstrap-icons";
+import { Bell, CalendarDay, Clock, Palette, X } from "react-bootstrap-icons";
+import {
+  DatePicker,
+  TimePicker,
+  MuiPickersUtilsProvider,
+} from "material-ui-pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 function AddNewTodo() {
   const [showModal, setShowModal] = useState(false);
   const [text, setText] = useState("");
+  const [day, setDay] = useState(new Date());
+  const [time, setTime] = useState(new Date());
 
   return (
     <div className="AddNewTodo">
@@ -13,29 +21,55 @@ function AddNewTodo() {
       </div>
 
       <Modal showModal={showModal} setShowModal={setShowModal}>
-        <form>
-          <div classNam="text">
-            <h3>Add New to do!</h3>
-            <input
-              type="text"
-              value={text}
-              onChange={(e) => setText(e.target.value)}
-              placeholder="To do ..."
-              autoFocus
-            />
-          </div>
-          <div className="remind">
-            <Bell />
-            <p>Remind Me!</p>
-          </div>
-          <div className="pick-day">
-            <div className="title">
-              <CalendarDay />
-              <p>Choose a day</p>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <form>
+            <div className="text">
+              <h3>Add New to do!</h3>
+              <input
+                type="text"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                placeholder="To do ..."
+                autoFocus
+              />
             </div>
-            date-picker
-          </div>
-        </form>
+            <div className="remind">
+              <Bell />
+              <p>Remind Me!</p>
+            </div>
+            <div className="pick-day">
+              <div className="title">
+                <CalendarDay />
+                <p>Choose a day</p>
+              </div>
+              <DatePicker />
+            </div>
+            <div className="pick-time">
+              <div className="title">
+                <Clock />
+                <p>Choose time</p>
+              </div>
+              <TimePicker />
+            </div>
+            <div className="pick-project">
+              <div className="title">
+                <Palette />
+                <p>Choose a project</p>
+              </div>
+              <div className="projects">
+                <div className="project active">personal</div>
+
+                <div className="project">work</div>
+              </div>
+            </div>
+            <div className="cancel" onClick={() => setShowModal(false)}>
+              <X size="40" />
+            </div>
+            <div className="confirm">
+              <button>+ Add to do</button>
+            </div>
+          </form>
+        </MuiPickersUtilsProvider>
       </Modal>
     </div>
   );
